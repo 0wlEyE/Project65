4bc7ccf67e710a29c4e9fd6c966390085b37b66d
F4n&4zY <65070187@kmitl.ac.th>
2022-11-28 15:35:49 +0700

    Add simple calculate function

--- project65_FullGUI.py
+++ project65_FullGUI.py
@@ -19,6 +19,17 @@ def clear(): # <--------- เคลียร์ข้อมูลทั้งห
     equation = ""
     label_result.config(text=equation)
 
+def calculate():
+    global equation
+    result = ""
+    if equation != "":
+        try:
+            result = eval(equation)
+        except:
+            result = "Error"
+            equation = ""
+        label_result.config(text=result)
+
 label_result = Label(root, width=25,height=2,text="",font=("arial",30))
 label_result.pack()
 # ^ ^ ^ ช่องแสดงตัวเลข
@@ -29,13 +40,13 @@ Button(root,text="C", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff
 #  ปุ่ม       ชื่อปุ่ม         ขนาดของปุ่ม       ฟอนต์                                          สี         ตำแหน่งของปุ่ม
 Button(root,text="//", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: show("//")).place(x=150,y=100)   # //
 Button(root,text="%", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: show("%")).place(x=290,y=100)     # %
-Button(root,text="÷", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: show("÷")).place(x=430,y=100)     # ÷
+Button(root,text="÷", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: show("/")).place(x=430,y=100)     # ÷
 
 
 Button(root,text="7", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("7")).place(x=10,y=200)      # 7
 Button(root,text="8", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("8")).place(x=150,y=200)     # 8
 Button(root,text="9", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("9")).place(x=290,y=200)     # 9
-Button(root,text="x", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: show("x")).place(x=430,y=200)     # x
+Button(root,text="x", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: show("*")).place(x=430,y=200)     # x
 
 Button(root,text="4", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("4")).place(x=10,y=300)      # 4
 Button(root,text="5", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("5")).place(x=150,y=300)     # 5
@@ -50,7 +61,7 @@ Button(root,text="+", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff
 Button(root,text="+/-", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("+/-")).place(x=10,y=500)  # +/-
 Button(root,text="0", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show("0")).place(x=150,y=500)     # 0
 Button(root,text=".", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#2a2d36",command=lambda: show(".")).place(x=290,y=500)     # .
-Button(root,text="=", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: show("=")).place(x=430,y=500)     # =
+Button(root,text="=", width=5, height=1, font=("arial",30,"bold"), bd=1,fg="#fff",bg="#fe9037",command=lambda: calculate()).place(x=430,y=500)     # =
 #------------------------------------------------------------------------------------------------------------------
 
 root.mainloop()
